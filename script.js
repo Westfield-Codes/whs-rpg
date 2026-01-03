b1561ebb57cd1dc139e4568fb3fadfd355ee7276
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2026-01-03 11:31:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+/* Global Variables */
 const player = new Player();
-
 let xp = player.xp;
 let health = player.health;
 let gold = player.gold;
@@ -7,63 +7,8 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
-
 let objects = ["orb", "key", "skull"]
 
-const controls = document.getElementById("controls");
-const button1 = document.querySelector("#controls :nth-child(1)");
-const container = document.querySelector('.container:nth-child(2)');
-const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3");
-const text = document.querySelector("#text");
-const xpText = document.querySelector("#xpText");
-const healthText = document.querySelector("#healthText");
-const goldText = document.querySelector("#goldText");
-const monsterStats = document.querySelector("#monsterStats");
-const monsterNameText = document.querySelector("#monsterName");
-const monsterHealthText = document.querySelector("#monsterHealth");
-const myObjects = document.querySelector("#stats li:nth-child(4)"); 
-const myWeapons = document.querySelector("#stats li:nth-child(5)");
-const stats = document.querySelector("#stats li:nth-child(5)");
-const infoBox = document.getElementById('infoBox');
-function showObjects() {
-	showInventory(myObjects, objects, "objects");
-	
-}
-
-function showName(){
-	document.getElementById("playerName").innerHTML=player.charName;
-	document.getElementById("picture").innerHTML="<img src=\""+ player.image+"\">";
-}
-
-function showWeapons(){
-	console.log(currentWeapon)
-	showInventory(myWeapons, buildWeapons(), "weapons")
-
-}
-function showInventory(container, items, listName){
-	// Toggle the inventory list: if it already exists, remove it; otherwise create it
-	const existing = container.querySelector(`#${listName}`);
-	if (existing) {
-		existing.remove();
-		return;
-	}
-	let inventoryList = document.createElement("ul");
-	inventoryList.id = listName;
-	for (let i = 0; i < items.length; i++){
-		let newItem = document.createElement("li");
-		newItem.textContent = items[i];
-		inventoryList.appendChild(newItem);
-	}
-	container.appendChild(inventoryList);
-}
-function buildWeapons(){
-	let w = [];
-	for(let weapon = 0; weapon<player.weapons.length;weapon++){
-		w.push(allWeapons[weapon].name)
-	}
-	return w;
-}
 
 const allWeapons = [
 	{
@@ -153,35 +98,6 @@ const locations = [
 	}
 ]
 
-
-// initialize buttons
-button1.onclick = goStore;
-button2.onclick = goCave;
-button3.onclick = fightDragon;
-
-function update(location) {
-    monsterStats.style.display = "none";
-	button1.innerText = location["button text"][0];
-	button2.innerText = location["button text"][1];
-	button3.innerText = location["button text"][2];
-	button1.onclick = location["button functions"][0];
-	button2.onclick = location["button functions"][1];
-	button3.onclick = location["button functions"][2];
-    text.innerText = location.text;    
-}
-
-function goTown() {
-    update(locations[0]);
-}
-
-function goStore() {
-    update(locations[1]);
-}
-
-function goCave() {
-    update(locations[2]);
-}
-
 function buyHealth() {
     if (gold >= 10) {
         gold -= 10;
@@ -193,7 +109,6 @@ function buyHealth() {
     }
 
 }
-
 
 function sellWeapon() {
 	if (inventory.length > 1) {
